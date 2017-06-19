7c6f34e96815e217cf69abbda3346ad0ebb0dad3
mach0319 <mach0319@naver.com>
2017-06-20 01:39:13 +0900

    calc_finished

--- Calc/app/src/main/java/me/mach0319/calc/MainActivity.java
+++ Calc/app/src/main/java/me/mach0319/calc/MainActivity.java
@@ -2,12 +2,56 @@ package me.mach0319.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ed1,ed2;
+    Button pl,mi,gop,na;
+    TextView tx;
+    View.OnClickListener clickListener;
+    double a,b,c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pl = (Button)findViewById(R.id.btpl);
+        mi = (Button)findViewById(R.id.btmi);
+        gop = (Button)findViewById(R.id.btgop);
+        na = (Button)findViewById(R.id.btna);
+        ed1 = (EditText)findViewById(R.id.ed1);
+        ed2 = (EditText)findViewById(R.id.ed2);
+        tx = (TextView)findViewById(R.id.tx);
+
+       clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Double.parseDouble(ed1.getText().toString());
+                b = Double.parseDouble(ed2.getText().toString());
+                switch (v.getId()) {
+                    case R.id.btpl:
+                        c = a + b;
+                        break;
+                    case R.id.btmi:
+                        c = a - b;
+                        break;
+                    case R.id.btna:
+                        c = a / b;
+                        break;
+                    case R.id.btgop:
+                        c = a / b;
+                        break;
+                }
+
+                tx.setText(String.valueOf(c));
+            }
+       };
+
+        pl.setOnClickListener(clickListener);
+        mi.setOnClickListener(clickListener);
+        gop.setOnClickListener(clickListener);
+        na.setOnClickListener(clickListener);
     }
 }
