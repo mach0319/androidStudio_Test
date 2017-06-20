f8d90b317ccfdbc14789fbceae1704b320dc3fa3
mach0319 <mach0319@naver.com>
2017-06-20 09:31:07 +0900

    test_1

--- firebasetest/app/src/main/java/me/mach0319/firebasetest/MainActivity.java
+++ firebasetest/app/src/main/java/me/mach0319/firebasetest/MainActivity.java
@@ -2,12 +2,26 @@ package me.mach0319.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listview;
+    EditText editText;
+    Button sendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview = (ListView)findViewById(R.id.listview);
+        editText = (EditText)findViewById(R.id.editText);
+        sendButton = (Button)findViewById(R.id.button);
+        userName = "user" + new Random().nextInt(10000);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        listView.setAdapter(adapter);
     }
 }
